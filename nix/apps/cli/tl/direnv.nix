72711cc0d79535d5cf02bb4a4e2bbf6eebb957bd
yehvaed <20567435+yehvaed@users.noreply.github.com>
2025-08-04 09:35:47 +0000

    feat(direnv): add app

---
+++ nix/apps/cli/tl/direnv.nix
@@ -0,0 +1,13 @@
+{
+  nix-config.apps.direnv = {
+    home = {
+      programs.direnv = {
+        enable = true;
+        enableZshIntegration = true;
+        nix-direnv.enable = true;
+      };
+    };
+
+    tags = [ "tl" ];
+  };
+}
