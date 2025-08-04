4bb5669ef24e8395278b1bdbc420589f289b51fe
yehvaed <20567435+yehvaed@users.noreply.github.com>
2025-08-04 06:57:42 +0200

    feat: add host specific overrides

--- nix/hosts/mthrshp/default.nix
+++ nix/hosts/mthrshp/default.nix
@@ -1,23 +1,39 @@
-{ lib, ... }: {
-  nix-config.hosts.mthrshp = rec {
-    nixos = import ./nixos/configuration.nix;
-    home = import ./nixos/home.nix;
+{ lib, ... }: let
+host = "mthrshp";
 
-    tags = {
-      cntr = true;
-      nix = true;
-      scm = true;
-      tl = true;
+in {
+  nix-config = {
+    apps = {
+      "${host}@kde" = {
+          nixos = {
+            services = {
+              desktopManager.plasma6.enable = true;
+              displayManager.sddm.enable = true;
+              displayManager.sddm.wayland.enable = true;
+            };
+          };
+
+          tags = [ "${host}" ]; 
+      };
     };
 
-    nix-config = {
-      apps.kde.enable = true;
+    hosts.${host} = rec {
+      kind = "nixos";
+      system = "x86_64-linux";
+
+      username = "yehvaed";
+      homeDirectory = "/home/${username}";
+
+      tags = {
+        cntr = true;
+        nix = true;
+        scm = true;
+        tl = true;
+        ${host} = true;
+      };
+
+      nixos = import ./nixos/configuration.nix;
+      home = import ./nixos/home.nix;
     };
-
-    kind = "nixos";
-    system = "x86_64-linux";
-
-    username = "yehvaed";
-    homeDirectory = "/home/${username}";
   };
 }
--- nix/hosts/mthrshp/nixos/home.nix
+++ nix/hosts/mthrshp/nixos/home.nix
@@ -1,3 +1,1 @@
-{ ... } : {
-  home.stateVersion = "24.05";
-}
+ { ... }: { home.stateVersion = "24.05"; };
